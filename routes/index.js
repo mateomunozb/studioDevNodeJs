d4bc6a7646cda57f01a22886e234183225a40aab
root <root@LAPTOP-HABG4AFK.localdomain>
2020-03-30 00:29:11 -0500

    Method post to forms data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 const express = require('express')
+const url = require('url')
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.render('index')
+  //   console.log('req.query', req)
+  if (req.query) {
+    const formCookie = req.cookies['contact']
+    console.log('formCookie', formCookie)
+    const query = req.query
+    console.log('query', query)
+    res.render('index', { query, formCookie })
+  } else {
+    res.render('index')
+  }
+})
+
+router.post('/', (req, res) => {
+  console.log('TCL: req', req.body)
+  const { email, message } = req.body
+
+  const formContent = { email: email, msg: message }
+
+  res.cookie('contact', formContent, { maxAge: 900000, httpOnly: true })
+  res.redirect(
+    302,
+    url.format({
+      pathname: '/',
+      query: {
+        email: !email ? 0 : 1,
+        msg: !message ? 0 : 1,
+        cont: '#contacto'
+      }
+    })
+  )
 })
 
 module.exports = router
