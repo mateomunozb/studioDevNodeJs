89444eb3e71534b875b41aa9e090d45c39e50a49
root <root@LAPTOP-HABG4AFK.localdomain>
2020-03-30 00:27:38 -0500

    Add aditional configurations

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 require('dotenv').config()
 const express = require('express')
-
-const portNumber = process.env.PORT || 3000
-
-const app = express()
+const bodyparser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
 const routes = require('./routes/index')
 
+const portNumber = process.env.PORT || 3000
+const app = express()
+
+app.use(cookieParser())
+app.use(bodyparser.urlencoded({ extended: false }))
 app.use(express.static('public'))
 app.use('/', routes)
 
